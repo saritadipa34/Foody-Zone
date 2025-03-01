5e72c40eada144cd2558481a8d33a9080a7cace5
saritadipa34 <saritadipa34@gmail.com>
2025-03-01 12:56:24 +0530

    Initialized with vite

---
+++ app/src/App.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const App= ()=>{
+  return(
+  <Container>
+    div
+  </Container>
+  )
+}
+export default App;
+
+constbContainer=styled.div`
+background`;
