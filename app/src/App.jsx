ee9bbcb9210469c8a39977ba6969559c78d4811d
saritadipa34 <saritadipa34@gmail.com>
2025-03-02 13:46:02 +0530

    WIP:Top section in progress  with styled components

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,11 +3,67 @@ import styled from "styled-components"
 const App= ()=>{
   return(
   <Container>
-    div
+<TopSection>
+<Box>
+  <div>
+<Image src="/logo.jpeg" alt="logo" />
+  </div>
+  <div>
+<Input  placeholder="Search for food" type="text" />
+  </div>
+  </Box>
+ <Lists>
+      <Items>All</Items>
+      <Items>Breakfast</Items>
+      <Items>Lunch</Items>
+      <Items>Dinner</Items>
+      </Lists>
+</TopSection>
   </Container>
   )
 }
 export default App;
 
-constbContainer=styled.div`
-background`;
+const Container =styled.div`
+
+`;
+const TopSection=styled.section`
+height:100px;
+max-width:100%;
+background-color:gray;
+padding:20px;
+`
+
+const Box = styled.div`
+display:flex;
+justify-content:space-between;
+margin:0 auto`;
+
+
+const Image=styled.img`
+height:50px;
+width:100px;
+`;
+
+const Lists=styled.div`
+width:200px;
+margin:0 auto;
+display:flex;
+list-style-type:none;
+gap:5px;
+`;
+
+const Input=styled.input`
+outline:0;
+padding:5px;
+`
+
+const Items=styled.button`
+border:1px solid black;
+background-color:#FFA500;
+padding:2px;
+border-radius:5px;
+// margin-inline:3px;
+cursor:pointer;
+`;
+
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createGlobalStyle } from 'styled-components'
 import App from './App.jsx'
 
+const GlobalStyle=createGlobalStyle`
+
+*{
+margin:0;
+padding:0;
+box-sizing:border-box;
+}
+
+body{
+height:100%;
+width:100%;
+background-color:sky;
+overflow:hidden;
+}
+`
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>,
 )
